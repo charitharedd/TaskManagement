96e89f92bb40a8c375ea40282458d001407b793a
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-12-09 16:34:38 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,8 @@
-
+// models/user.js
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+  });
+  return User;
+};
