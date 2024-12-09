850f28e0b32269e8f87b3f266723a186cba59ead
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-12-09 16:36:52 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,15 @@
 
+// middleware/auth.js
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(403).json({ error: 'Unauthorized' });
+
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ error: 'Invalid token' });
+    req.user = user;
+    next();
+  });
+};
